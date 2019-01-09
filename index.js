34aa6ed4a4e0291d604348412d68c4b57447c02f
John Hayes <johnnyhayes7@gmail.com>
2019-01-09 21:03:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function createRock(x) {
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
      */
-     else if(top < GAME_HEIGHT){
+    if(top < GAME_HEIGHT){
       window.requestAnimationFrame(moveRock);
     }
 
