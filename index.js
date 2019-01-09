522c5a4f9d2ac4eb48b7982e3986141b02a0bf24
John Hayes <johnnyhayes7@gmail.com>
2019-01-09 21:16:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -112,6 +112,7 @@ function createRock(x) {
 function endGame() {
   while(ROCKS.length > 0){
     GAME.removeChild(ROCKS); 
+    rock.remove;
   }
   clearInterval(gameInterval);
   window.removeEventListener('keydown', moveDodger);
